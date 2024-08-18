724933ebe6a73688edf62f675b2fc8d95cbe3c21
Ivan Krstev <ivankrstev246@gmail.com>
2024-08-18 23:49:57 +0200

    init: add CLI project for command-line interface usage

---
+++ YouTubeDownloader.CLI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YouTubeDownloader.CLI
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
