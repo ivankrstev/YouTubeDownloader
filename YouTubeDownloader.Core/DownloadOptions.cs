dc566c82fa8858108235f740ee65dc4ff2e49f53
Ivan Krstev <ivankrstev246@gmail.com>
2024-08-21 22:07:23 +0200

    feat: added model for managing download options

---
+++ YouTubeDownloader.Core/DownloadOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace YouTubeDownloader.Core
+{
+    public class DownloadOptions
+    {
+        public required string Url { get; set; }
+        public required string VideoQuality { get; set; }
+        public required string AudioQuality { get; set; }
+        public required string Format { get; set; }
+        public required string OutputDirectory { get; set; }
+        public required bool IsPlaylist { get; set; }
+    }
+}
