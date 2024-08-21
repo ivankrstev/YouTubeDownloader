6bcbff5c86fff2d4d5755284cb38adbfbdd021b8
Ivan Krstev <ivankrstev246@gmail.com>
2024-08-21 22:09:27 +0200

    feat: add arguments options model in argument parser

---
+++ YouTubeDownloader.CLI/ArgumentParser.cs
@@ -0,0 +1,26 @@
+﻿using CommandLine;
+
+namespace YouTubeDownloader.CLI
+{
+    internal static class ArgumentParser
+    {
+    }
+
+    internal class Options
+    {
+        [Value(0, MetaName = "video-url", Required = true, HelpText = "The URL of the video to download.")]
+        public required string VideoUrl { get; set; }
+
+        [Option('o', "output-directory", Default = ".", HelpText = "Set the output directory.")]
+        public required string OutputDirectory { get; set; } = ".";
+
+        [Option('v', "video-quality", Default = "highest-available", HelpText = "Set the video quality.")]
+        public string VideoQuality { get; set; } = "highest-available";
+
+        [Option('a', "audio-quality", Default = "highest-available", HelpText = "Set the audio quality.")]
+        public string AudioQuality { get; set; } = "highest-available";
+
+        [Option('f', "format", Default = "mp4", HelpText = "Set the output format.")]
+        public string Format { get; set; } = "mp4";
+    }
+}
