8a147555e942486b81650f988c12d4b7e7322dcf
Ivan Krstev <ivankrstev246@gmail.com>
2024-08-22 02:21:21 +0200

    feat: add size format & filename sanitize functions

---
+++ YouTubeDownloader.Core/VideoDownloader.cs
@@ -0,0 +1,26 @@
+﻿namespace YouTubeDownloader.Core
+{
+    public class VideoDownloader
+    {
+        private static string SanitizeFileName(string fileName)
+        {
+            foreach (char c in Path.GetInvalidFileNameChars())
+            {
+                fileName = fileName.Replace(c, '_');
+            }
+            return fileName;
+        }
+
+        private static string FormatSize(long size)
+        {
+            string[] sizes = ["B", "KB", "MB", "GB", "TB"];
+            int order = 0;
+            while (size >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                size /= 1024;
+            }
+            return $"{size:0.##} {sizes[order]}";
+        }
+    }
+}
