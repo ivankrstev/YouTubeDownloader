f58c571a25e0c2ae32337837df7eea8a72138065
Ivan Krstev <ivankrstev246@gmail.com>
2024-08-22 02:22:14 +0200

    feat: add video download function with best audio & specific video quality

--- YouTubeDownloader.Core/VideoDownloader.cs
+++ YouTubeDownloader.Core/VideoDownloader.cs
@@ -1,7 +1,57 @@
-﻿namespace YouTubeDownloader.Core
+﻿using YoutubeExplode;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Videos.Streams;
+
+namespace YouTubeDownloader.Core
 {
     public class VideoDownloader
     {
+        private readonly YoutubeClient _youtube;
+
+        public VideoDownloader()
+        {
+            _youtube = new YoutubeClient();
+        }
+
+        public async Task DownloadVideoAsync(DownloadOptions downloadOptions)
+        {
+            var videoUrl = downloadOptions.Url;
+            var outputDirectory = downloadOptions.OutputDirectory;
+            var videoQuality = downloadOptions.VideoQuality;
+            var audioQuality = downloadOptions.AudioQuality;
+            var format = downloadOptions.Format;
+
+            var streamManifest = await _youtube.Videos.Streams.GetManifestAsync(videoUrl);
+            var videoMetadata = await _youtube.Videos.GetAsync(videoUrl);
+            var outputFilePath = Path.Combine(outputDirectory, $"{SanitizeFileName(videoMetadata.Title)}.mp4");
+            // get the audio stream with the highest bitrate
+            var audioStreamInfo = streamManifest
+                .GetAudioOnlyStreams()
+                .GetWithHighestBitrate();
+
+            // get the video stream with the specified quality, or the highest quality if not found
+            var videoStreamInfo = streamManifest
+                .GetVideoOnlyStreams()
+                .FirstOrDefault(s => s.VideoQuality.Label.StartsWith(videoQuality))
+                ?? streamManifest
+                .GetVideoOnlyStreams()
+                .GetWithHighestVideoQuality();
+
+            if (videoStreamInfo != null && audioStreamInfo != null)
+            {
+                Console.WriteLine($"- Title: {videoMetadata.Title}");
+                Console.WriteLine($"  Length: {videoMetadata.Duration} | Size: {FormatSize(audioStreamInfo.Size.Bytes + videoStreamInfo.Size.Bytes)}");
+                Console.WriteLine($"  Quality: {videoStreamInfo.VideoQuality.Label} | {audioStreamInfo.Bitrate}");
+                var streamInfos = new IStreamInfo[] { audioStreamInfo, videoStreamInfo };
+                await _youtube.Videos.DownloadAsync(streamInfos, new ConversionRequestBuilder(outputFilePath).Build());
+                Console.WriteLine("Downloaded!\n");
+            }
+            else
+            {
+                Console.WriteLine($"No suitable stream found for: {videoMetadata.Title}");
+            }
+        }
+
         private static string SanitizeFileName(string fileName)
         {
             foreach (char c in Path.GetInvalidFileNameChars())
