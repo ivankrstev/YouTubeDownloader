40f41632a07d4c575f802981d4777d72f1d0b9bb
Ivan Krstev <ivankrstev246@gmail.com>
2024-08-22 03:02:30 +0200

    feat: connect the playlist and video downloader to the cli program starter

--- YouTubeDownloader.CLI/Program.cs
+++ YouTubeDownloader.CLI/Program.cs
@@ -1,14 +1,24 @@
-﻿namespace YouTubeDownloader.CLI
+﻿using YouTubeDownloader.Core;
+
+namespace YouTubeDownloader.CLI
 {
     internal static class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
             var downloadOptions = ArgumentParser.ParseArguments(args);
             if (downloadOptions == null)
             {
                 Console.WriteLine("Invalid arguments provided.");
                 Environment.Exit(1);
+            }
+            else if (downloadOptions.IsPlaylist)
+            {
+                await new PlaylistDownloader(downloadOptions).DownloadPlaylistAsync(downloadOptions.Url);
+            }
+            else
+            {
+                await new VideoDownloader().DownloadVideoAsync(downloadOptions);
             }
         }
     }
