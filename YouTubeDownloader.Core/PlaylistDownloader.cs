5e022e17b569aebfd3368d497a55cb263a37938b
Ivan Krstev <ivankrstev246@gmail.com>
2024-08-22 02:59:40 +0200

    feat: add full playlist download function using video downloader

---
+++ YouTubeDownloader.Core/PlaylistDownloader.cs
@@ -0,0 +1,29 @@
+﻿using YoutubeExplode;
+using YoutubeExplode.Common;
+
+namespace YouTubeDownloader.Core
+{
+    public class PlaylistDownloader(DownloadOptions downloadOptions)
+    {
+        private readonly YoutubeClient _youtube = new();
+        private readonly VideoDownloader _videoDownloader = new();
+        private readonly DownloadOptions _downloadOptions = downloadOptions;
+
+        public async Task DownloadPlaylistAsync(string playlistUrl)
+        {
+            // Get playlist information
+            var playlist = await _youtube.Playlists.GetAsync(playlistUrl);
+            Console.WriteLine($"Playlist: {playlist.Title}");
+            // Get videos in the playlist
+            var videos = await _youtube.Playlists.GetVideosAsync(playlistUrl);
+            Console.WriteLine(videos.Count + " videos found.");
+            // Download video by video
+            foreach (var video in videos)
+            {
+                _downloadOptions.Url = video.Url;
+                await _videoDownloader.DownloadVideoAsync(_downloadOptions);
+            }
+            Console.WriteLine("Playlist download complete.");
+        }
+    }
+}
