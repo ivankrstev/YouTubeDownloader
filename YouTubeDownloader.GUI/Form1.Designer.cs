2e4551ecd48c49aa3689e978c3cf97e31d958997
Ivan Krstev <ivankrstev246@gmail.com>
2024-08-23 03:01:09 +0200

    feat: add split container layout - 2 columns & dimensions adjustments to form

--- YouTubeDownloader.GUI/Form1.Designer.cs
+++ YouTubeDownloader.GUI/Form1.Designer.cs
@@ -28,12 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            MainSplitContainer = new SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)MainSplitContainer).BeginInit();
+            MainSplitContainer.SuspendLayout();
+            SuspendLayout();
+            // 
+            // MainSplitContainer
+            // 
+            MainSplitContainer.Dock = DockStyle.Fill;
+            MainSplitContainer.Location = new Point(10, 10);
+            MainSplitContainer.MinimumSize = new Size(300, 300);
+            MainSplitContainer.Name = "MainSplitContainer";
+            MainSplitContainer.Panel1MinSize = 100;
+            MainSplitContainer.Panel2MinSize = 100;
+            MainSplitContainer.Size = new Size(780, 430);
+            MainSplitContainer.SplitterDistance = 390;
+            MainSplitContainer.SplitterWidth = 1;
+            MainSplitContainer.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(MainSplitContainer);
+            MinimumSize = new Size(600, 300);
+            Name = "Form1";
+            Padding = new Padding(10);
+            Text = "Youtube Downloader";
+            ((System.ComponentModel.ISupportInitialize)MainSplitContainer).EndInit();
+            MainSplitContainer.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private SplitContainer MainSplitContainer;
     }
 }
