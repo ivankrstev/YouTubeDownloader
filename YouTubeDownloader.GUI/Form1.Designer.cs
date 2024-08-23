26df148271f40132ecc50517b0f03505c29fc9b3
Ivan Krstev <ivankrstev246@gmail.com>
2024-08-24 01:01:30 +0200

    feat: add label & textbox for video url in table layout to left split panel

--- YouTubeDownloader.GUI/Form1.Designer.cs
+++ YouTubeDownloader.GUI/Form1.Designer.cs
@@ -29,8 +29,15 @@
         private void InitializeComponent()
         {
             MainSplitContainer = new SplitContainer();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)MainSplitContainer).BeginInit();
+            MainSplitContainer.Panel1.SuspendLayout();
             MainSplitContainer.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // MainSplitContainer
@@ -39,12 +46,59 @@
             MainSplitContainer.Location = new Point(10, 10);
             MainSplitContainer.MinimumSize = new Size(300, 300);
             MainSplitContainer.Name = "MainSplitContainer";
+            // 
+            // MainSplitContainer.Panel1
+            // 
+            MainSplitContainer.Panel1.Controls.Add(tableLayoutPanel1);
             MainSplitContainer.Panel1MinSize = 100;
             MainSplitContainer.Panel2MinSize = 100;
             MainSplitContainer.Size = new Size(780, 430);
             MainSplitContainer.SplitterDistance = 390;
             MainSplitContainer.SplitterWidth = 1;
             MainSplitContainer.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Size = new Size(390, 430);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(384, 101);
+            panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(30, 50);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Paste the URL or ID here...";
+            textBox1.Size = new Size(321, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(244, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Enter the YouTube video or playlist URL or ID:";
             // 
             // Form1
             // 
@@ -56,13 +110,21 @@
             Name = "Form1";
             Padding = new Padding(10);
             Text = "Youtube Downloader";
+            MainSplitContainer.Panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)MainSplitContainer).EndInit();
             MainSplitContainer.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private SplitContainer MainSplitContainer;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel1;
+        private TextBox textBox1;
+        private Label label1;
     }
 }
