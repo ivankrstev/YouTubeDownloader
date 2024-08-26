d071fe343ecc14104eca1e6006e034285cf610cc
Ivan Krstev <ivankrstev246@gmail.com>
2024-08-26 22:28:18 +0200

    refactor: moved youtube input processing to download options model in core project

--- YouTubeDownloader.CLI/ArgumentParser.cs
+++ YouTubeDownloader.CLI/ArgumentParser.cs
@@ -1,5 +1,6 @@
 ﻿using CommandLine;
 using YouTubeDownloader.Core;
+using static YouTubeDownloader.Core.DownloadOptions;
 
 namespace YouTubeDownloader.CLI
 {
@@ -66,31 +67,6 @@ namespace YouTubeDownloader.CLI
                     Environment.Exit(1);
                 });
             return downloadOptions;
-        }
-
-        private static (bool isPlaylist, string normalizedUrl) ProcessYouTubeInput(string input)
-        {
-            if (Uri.TryCreate(input, UriKind.Absolute, out _))
-            {
-                // assume it's a URL
-                if (input.Contains("playlist") || input.Contains("list="))
-                    return (true, input);
-                return (false, input);
-            }
-            else if (input.Length == 11)
-            {
-                // Assume it's a video ID
-                return (true, $"https://www.youtube.com/watch?v={input}");
-            }
-            else if (input.Length == 34 && (input.StartsWith("PL") || input.StartsWith("UU") || input.StartsWith("LL")))
-            {
-                // Assume it's a playlist ID
-                return (true, $"https://www.youtube.com/playlist?list={input}");
-            }
-            else
-            {
-                throw new ArgumentException("Invalid YouTube URL, video ID, or playlist ID.");
-            }
         }
     }
 
--- YouTubeDownloader.Core/DownloadOptions.cs
+++ YouTubeDownloader.Core/DownloadOptions.cs
@@ -8,5 +8,30 @@
         public required string Format { get; set; }
         public required string OutputDirectory { get; set; }
         public required bool IsPlaylist { get; set; }
+
+        public static (bool isPlaylist, string normalizedUrl) ProcessYouTubeInput(string input)
+        {
+            if (Uri.TryCreate(input, UriKind.Absolute, out _))
+            {
+                // assume it's a URL
+                if (input.Contains("playlist") || input.Contains("list="))
+                    return (true, input);
+                return (false, input);
+            }
+            else if (input.Length == 11)
+            {
+                // Assume it's a video ID
+                return (true, $"https://www.youtube.com/watch?v={input}");
+            }
+            else if (input.Length == 34 && (input.StartsWith("PL") || input.StartsWith("UU") || input.StartsWith("LL")))
+            {
+                // Assume it's a playlist ID
+                return (true, $"https://www.youtube.com/playlist?list={input}");
+            }
+            else
+            {
+                return (false, "");
+            }
+        }
     }
 }
