e323b9a392d379cfdbc64079fe1eb91787da1b8f
Ivan Krstev <ivankrstev246@gmail.com>
2024-09-01 23:58:10 +0200

    feat: add separate function for fetching playlist & its videos metadata

--- YouTubeDownloader.Core/PlaylistDownloader.cs
+++ YouTubeDownloader.Core/PlaylistDownloader.cs
@@ -11,19 +11,25 @@ namespace YouTubeDownloader.Core
 
         public async Task DownloadPlaylistAsync(string playlistUrl)
         {
-            // Get playlist information
-            var playlist = await _youtube.Playlists.GetAsync(playlistUrl);
-            Console.WriteLine($"Playlist: {playlist.Title}");
-            // Get videos in the playlist
-            var videos = await _youtube.Playlists.GetVideosAsync(playlistUrl);
-            Console.WriteLine(videos.Count + " videos found.");
+            // Get playlist and videos metadata
+            var (playlistMetadata, videosMetadata) = await GetPlaylistMetadataAndVideos(playlistUrl);
+            Console.WriteLine($"Playlist: {playlistMetadata.Title}");
+            Console.WriteLine(videosMetadata.Count + " videos found.");
             // Download video by video
-            foreach (var video in videos)
+            foreach (var video in videosMetadata)
             {
                 _downloadOptions.Url = video.Url;
                 await _videoDownloader.DownloadVideoAsync(_downloadOptions);
             }
             Console.WriteLine("Playlist download complete.");
+        }
+
+        // write me a function to get playlist metadata togeher with the metadata of videos in the playlist(title, duration) in one function:
+        public async Task<(YoutubeExplode.Playlists.Playlist, IReadOnlyList<YoutubeExplode.Playlists.PlaylistVideo>)> GetPlaylistMetadataAndVideos(string playlistUrl)
+        {
+            var playlist = await _youtube.Playlists.GetAsync(playlistUrl);
+            var videos = await _youtube.Playlists.GetVideosAsync(playlistUrl);
+            return (playlist, videos);
         }
     }
 }
